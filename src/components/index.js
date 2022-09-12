7902de5e8bbce14a9354b00f531b51735d12c4ea
Vann Liu <vann@cpcptw>
2022-09-12 21:07:48 +0800

    Fixed jsx components autoload

--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,22 @@
-import { defineAsyncComponent } from "vue"
+import { defineAsyncComponent } from 'vue'
 
 // 以 file system 自動引入
 const componentModules = import.meta.glob('./**/*.{vue,jsx}')
 const components = {}
 
 
-for (const path in componentModules) {
+for (const path in componentModules){
 
-    // icons 是在 Icon.vue 裡面用 component:is 的方式引入，故跳過
-    if( path.includes('./__icons') ){
-        continue
-    }
+  // icons 是在 Icon.vue 裡面用 component:is 的方式引入，故跳過
+  if( path.includes('./__icons') ){
+    continue
+  }
 
-    let componentName = path.replaceAll('./', '').replaceAll('.vue', '').replaceAll('/', '')
+  let componentName = path.replaceAll('./', '').replaceAll('.vue', '').replaceAll('.jsx', '').replaceAll('/', '')
 
-    components[componentName] = defineAsyncComponent({
-        loader: componentModules[/* @vite-ignore */ `${path}`]
-    })
+  components[componentName] = defineAsyncComponent({
+    loader: componentModules[/* @vite-ignore */ `${path}`]
+  })
 }
 
 
