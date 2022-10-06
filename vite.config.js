7dbb3eb019e080eb72e772f2e30ae0f172c06628
Vann Liu <vann@cpcptw>
2022-10-06 14:14:19 +0800

    upgrade modules (+1 squashed commit) Squashed commits: [04f57f3] upgrade modules

--- vite.config.js
+++ vite.config.js
@@ -50,9 +50,9 @@ export default defineConfig(({mode})=>{
         enableObjectSlots: true,
         // options are passed on to @vue/babel-plugin-jsx
       }),
-      legacy({
-        targets: ['defaults', 'not IE 11']
-      }),
+      // legacy({
+      //   targets: ['defaults', 'not IE 11']
+      // }),
       visualizer()
     ],
     css: {
