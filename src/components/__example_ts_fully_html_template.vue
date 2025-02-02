d9966ce6298345c2fde02a2a4aeba03cc809954f
Vann Liu <vann@cpcp.tw>
2025-02-02 19:44:06 +0800

    feat: add example for html template

---
+++ src/components/__example_ts_fully_html_template.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { reactive, nextTick } from 'vue'
+import { useStore } from 'vuex'
+import { isEmpty } from '@src/helpers'
+
+interface IProps {}
+const props = defineProps<IProps>()
+// defineProps<IProps>() 和 withDefaults(defineProps<IProps>(), {}）請擇一使用，有要指定預設值的話，才使用後者，因為若 withDefaults 內的 props 屬性是空的話，會編譯失敗。
+// const props = withDefaults(defineProps<IProps>(), {})
+const store = useStore()
+const state = reactive({})
+</script>
+
+<template>
+  <slot
+  :store="store"
+  :state="state"
+  :isEmpty="isEmpty"
+  :nextTick="nextTick"></slot>
+</template>
